b08320dd735ce0905888ac28df0314695cbca460
ExOwLcIsT <demchenkovlad80@gmail.com>
2023-10-23 11:54:18 +0300

    test

---
+++ src/task1/Product.java
@@ -0,0 +1,16 @@
+package task1;
+
+public class Product extends Detail {
+    String material;
+
+    public Product(String name, int weight, String material) {
+        super(name, weight);
+        this.material = material;
+    }
+
+    @Override
+    public void show() {
+        super.show();
+        System.out.println("Матеріал: " + material);
+    }
+}
